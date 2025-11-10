f2bee3a134f934d3ce1144da4034d559daf2e05d
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-11-10 10:41:11 +0100

    initial commit

---
+++ src/main/java/com/example/SkolSystem_API/SkolSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkolSystem_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkolSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkolSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkolSystem_API/SkolSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkolSystem_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkolSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
