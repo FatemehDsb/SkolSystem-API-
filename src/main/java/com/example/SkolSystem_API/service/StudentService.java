d608907e89af34863e66174e86532a117a31efd1
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-11-10 14:56:26 +0100

    Add getAllStudents in controller, service and repo

--- src/main/java/com/example/SkolSystem_API/service/StudentService.java
+++ src/main/java/com/example/SkolSystem_API/service/StudentService.java
@@ -1,4 +1,22 @@
 package com.example.SkolSystem_API.service;
 
+import com.example.SkolSystem_API.model.Student;
+import com.example.SkolSystem_API.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Student> getAllStudents(){
+        return repository.getAllStudents();
+    }
+
 }
