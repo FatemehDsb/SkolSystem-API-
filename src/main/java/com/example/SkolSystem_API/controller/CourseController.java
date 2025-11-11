1c8928b6dd828129e6bf1dbda377f306d8051e3a
aletheos <aletheos@outlookcom>
2025-11-11 14:52:17 +0100

    Make changes to course controller > Change endpoint to the agreed upon plural form > Make controller call into its service > Make controller return DTO instead of model

--- src/main/java/com/example/SkolSystem_API/controller/CourseController.java
+++ src/main/java/com/example/SkolSystem_API/controller/CourseController.java
@@ -1,6 +1,6 @@
 package com.example.SkolSystem_API.controller;
 
-import com.example.SkolSystem_API.model.Course;
+import com.example.SkolSystem_API.dto.CourseDTO;
 import com.example.SkolSystem_API.service.CourseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.*;
  * <li> Forwards processed requests to the Business Layer for further logic.
  */
 @RestController
-@RequestMapping("api/course")
+@RequestMapping("/courses")
 public class CourseController {
     private CourseService service;
 
     @GetMapping
-    public ResponseEntity<Course> getCourses() {
-        return ResponseEntity.status(200).build();
+    public ResponseEntity<CourseDTO> getCourses() {
+        CourseDTO course = service.getCourses();
+        return ResponseEntity.ok(course);
     }
 }
