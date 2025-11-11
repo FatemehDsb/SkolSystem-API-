15ee8d1f40c4f00faf604d644e5a4c76eb904066
aletheos <aletheos@outlookcom>
2025-11-11 14:54:14 +0100

    Make program run again but annotating EnrollmentRepository with @Repository (Ignore this if already fixed)

--- src/main/java/com/example/SkolSystem_API/repository/EnrollmentRepository.java
+++ src/main/java/com/example/SkolSystem_API/repository/EnrollmentRepository.java
@@ -1,11 +1,13 @@
 package com.example.SkolSystem_API.repository;
 
 import com.example.SkolSystem_API.model.Enrollment;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class EnrollmentRepository {
 
     private List<Enrollment> enrollments = new ArrayList<>();
