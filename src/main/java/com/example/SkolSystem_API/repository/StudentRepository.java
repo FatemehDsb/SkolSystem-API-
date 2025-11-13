6716cfa89b952f84e113add3a0796988cf5e197c
Fatemeh Dastgheib <fatemeh.dastgheib@gmail.com>
2025-11-13 11:53:15 +0100

    update student entity

--- src/main/java/com/example/SkolSystem_API/repository/StudentRepository.java
+++ src/main/java/com/example/SkolSystem_API/repository/StudentRepository.java
@@ -1,43 +1,10 @@
 package com.example.SkolSystem_API.repository;
 
 import com.example.SkolSystem_API.model.Student;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-@Repository
-public class StudentRepository {
-    private List<Student> students = new ArrayList<>();
 
-    public StudentRepository(){
-        students.add(new Student(1, "Alice Johnson", 21, "alice.johnson@example.com"));
-        students.add(new Student(2, "Michael Smith", 23, "michael.smith@example.com"));
-        students.add(new Student(3, "Emma Williams", 20, "emma.williams@example.com"));
-        students.add(new Student(4, "James Brown", 22, "james.brown@example.com"));
-        students.add(new Student(5, "Olivia Davis", 19, "olivia.davis@example.com"));
-    }
-
-    public List<Student> findAll(){
-        return students;
-    }
-
-    public Optional<Student> findById(int id){
-        return students
-                .stream()
-                .filter( p -> p.getId() == id)
-                .findFirst();
-    }
-
-    public Student save(Student std){
-        int nextId = students.stream()
-                .mapToInt(s-> s.getId())
-                .max()
-                .orElse(0)+1;
-        std.setId(nextId);
-        students.add(std);
-        return std;
-
-    }
 }
