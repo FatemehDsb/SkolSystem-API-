f5d5b34f628e5d617dec08d1d3c422ebe7aca38f
aletheos <aletheos@outlookcom>
2025-11-14 13:43:33 +0100

    Add POST mapping to Course controller > Change toDTO methods to package private static > Add getById method to service > Change constructor parameters in model and dto(to allow the creation of objects with incomplete data)

--- src/main/java/com/example/SkolSystem_API/controller/CourseController.java
+++ src/main/java/com/example/SkolSystem_API/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.example.SkolSystem_API.controller;
 
 import com.example.SkolSystem_API.dto.CourseDTO;
 import com.example.SkolSystem_API.service.CourseService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,16 @@ public class CourseController {
     @GetMapping
     public ResponseEntity<List<CourseDTO>> getCourses() {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCourse(@Valid @RequestBody CourseDTO dto) {
+        service.createCourse(dto);
+        return ResponseEntity.ok("Course created successfully!");
     }
 }
